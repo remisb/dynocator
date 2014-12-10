8f378cb1fe090a17b200d7f2a1f67eb39a8473bf
Ahsanul Haque <ahsanul@gmail.com>
2014-12-10 12:35:12 +0600

    Moved server/mux to server.go

--- dyno/dynocator.go
+++ dyno/dynocator.go
@@ -40,32 +40,6 @@ func Refresh() {
 	Index()
 }
 
-func Serve() {
-	r := mux.NewRouter()
-	r.HandleFunc("/"+config.Admin, AdminIndex).Methods("GET")
-	r.HandleFunc("/"+config.Admin+"/add", AddGet).Methods("GET")
-	r.HandleFunc("/"+config.Admin+"/add", AddPost).Methods("POST")
-	r.HandleFunc("/"+config.Admin+"/settings", SettingsGet).Methods("GET")
-	r.HandleFunc("/"+config.Admin+"/settings", SettingsPost).Methods("POST")
-	r.HandleFunc("/"+config.Admin+"/edit/{post}", EditPost).Methods("GET")
-	r.HandleFunc("/"+config.Admin+"/edit/{post}", UpdatePost).Methods("POST")
-	r.HandleFunc("/"+config.Admin+"/uploadimage", UploadImage).Methods("POST")
-
-	r.HandleFunc("/category/{category}", Categories).Methods("GET")
-	r.HandleFunc("/categories", ListCategories).Methods("GET")
-
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir(config.Public)))
-
-	log.Printf("Server running on %s", ("http://localhost" + flags.Port))
-	log.Printf("Press ctrl+c to stop")
-
-	// Start file-change watcher for posts/
-	go ConvertWatcher()
-
-	http.Handle("/", r)
-	http.ListenAndServe(flags.Port, nil)
-}
-
 func CreateTemplate(name string, globpath string, w io.Writer, params map[interface{}]interface{}) {
 	tmpl := template.Must(template.New(name).Funcs(funcMap).ParseGlob(globpath))
 	tmpl.Execute(w, params)
---
+++ dyno/server.go
@@ -0,0 +1,33 @@
+package dyno
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func Serve() {
+	r := mux.NewRouter()
+	r.HandleFunc("/"+config.Admin, AdminIndex).Methods("GET")
+	r.HandleFunc("/"+config.Admin+"/add", AddGet).Methods("GET")
+	r.HandleFunc("/"+config.Admin+"/add", AddPost).Methods("POST")
+	r.HandleFunc("/"+config.Admin+"/settings", SettingsGet).Methods("GET")
+	r.HandleFunc("/"+config.Admin+"/settings", SettingsPost).Methods("POST")
+	r.HandleFunc("/"+config.Admin+"/edit/{post}", EditPost).Methods("GET")
+	r.HandleFunc("/"+config.Admin+"/edit/{post}", UpdatePost).Methods("POST")
+	r.HandleFunc("/"+config.Admin+"/uploadimage", UploadImage).Methods("POST")
+
+	r.HandleFunc("/category/{category}", Categories).Methods("GET")
+	r.HandleFunc("/categories", ListCategories).Methods("GET")
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(config.Public)))
+
+	log.Printf("Server running on %s", ("http://localhost" + flags.Port))
+	log.Printf("Press ctrl+c to stop")
+
+	// Start file-change watcher for posts/
+	go ConvertWatcher()
+
+	http.Handle("/", r)
+	http.ListenAndServe(flags.Port, nil)
+}
